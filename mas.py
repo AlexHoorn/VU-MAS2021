942ab8963eed7b1d49083ce4f47f6f8c937763ab
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2021-12-30 00:06:00 +0100

    Iterate search root

--- mas.py
+++ mas.py
@@ -1,5 +1,5 @@
 import random
-from functools import lru_cache
+from functools import cache
 from math import log, sqrt
 from time import perf_counter
 from typing import Dict, List, Tuple
@@ -8,6 +8,7 @@ from numpy import e
 
 
 def edit_distance(X: Tuple, Y: Tuple) -> int:
+    assert len(X) == len(Y)
     # Edit distance
     d = 0
     for x, y in zip(X, Y):
@@ -19,81 +20,104 @@ def edit_distance(X: Tuple, Y: Tuple) -> int:
 
 class BinaryTree:
     def __init__(self, depth: int, choices: List = ["L", "R"]) -> None:
-        # BinaryTree that generates rewards lazily so they can be of galactic size
+        # BinaryTree that can be of galactic size because it generates rewards lazily
 
         self.depth = int(depth)  # depth of the tree
         self.choices = list(choices)  # the choices at each node
 
-        self._target = self._generate_target()  # random path to base rewards on
+        self.target = self._generate_target()  # random path to base rewards on
         self.n_paths = len(choices) ** depth  # amount of paths in the tree
 
-    # Cache last million
-    @lru_cache(maxsize=1000000)
+    @cache
     def get_reward(self, path: Tuple) -> float:
         # Get a reward given a path
-        return self._reward_function(self._target, path)
+        return self._reward_function(self.target, path)
 
     def _generate_target(self):
         # Generate random path
         return tuple(random.choice(self.choices) for _ in range(self.depth))
 
-    def _reward_function(
-        self, target: Tuple, path: Tuple, B: float = 2.0, r: float = 20.0
-    ):
+    def _reward_function(self, target: Tuple, path: Tuple, B: float = 2, r: float = 20):
         return B * e ** -(edit_distance(target, path) / r)
 
 
 class MCTS:
-    def __init__(self, tree: BinaryTree, c, iterations=5, verbose=False) -> None:
+    def __init__(
+        self,
+        tree: BinaryTree,
+        c,
+        iterations=50,
+        max_patience=5,
+        rollouts=5,
+        verbose=False,
+    ) -> None:
         self.tree = tree  # the binary tree
-        self.c = c  # c for ucb
-        self.iterations = iterations  # allowed iterations for rollout
+        self.c = c  # c for ucb function
+        self.iterations = iterations  # iterations per setting of root
+        self.max_patience = max_patience  # so we can break iterations early
+        self.rollouts = rollouts  # allowed iterations for rollout
         self.verbose = verbose  # print at end of run
 
     def run(self):
+        path = tuple()  # new empty path
+        # Dictionaries to track rewards and n visits
+        t, n = {path: 0}, {path: 0}
+
         # Keep track of metrics
-        self.search_steps = 0
+        self.search_iterations = 0
+        self.early_stops = 0
         runtime = perf_counter()
 
-        # Dictionaries to track rewards and n visits
-        t, n = {(): 0}, {(): 0}
-
-        while True:
-            # Select current best path
-            path = self._select_path(t, n)
-            self.search_steps += 1
-
-            # Break if we found the leaf
-            if len(path) == self.tree.depth:
-                break
-
-            # Determine rewards for child nodes
-            for c in self.tree.choices:
-                curr_path = path + tuple(c)
-
-                t[curr_path] = self._roll_out(curr_path)
-                n[curr_path] = 1
-
-                # Back the values up
-                self._backup(curr_path, t)
-                self._backup(curr_path, n)
+        # Construct path by iteratively setting new roots
+        for _ in range(self.tree.depth):
+            path += tuple(self._determine_root(path, t, n))
 
         # Determine runtime
         runtime = perf_counter() - runtime
 
         # Store attributes after run
-        self.t = t
-        self.n = n
-        self.best = path
+        self.path = path
         self.reward = self.tree.get_reward(path)
         self.runtime = runtime
+        self.t = t
+        self.n = n
 
         # Print after run
         if self.verbose:
             reward = self.reward
             print(
-                f"Found {reward=:.2f} with path=`{''.join(path)}` in {runtime:.2f}s and {self.search_steps} steps"
+                f"Found {reward=:.2f} with path=`{''.join(path)}` in {runtime:.2f}s and {self.search_iterations} steps"
             )
+
+    def _determine_root(self, path: Tuple, t, n):
+        # Keep note of patience
+        patience = 0
+        # Iterate to max iterations
+        for _ in range(self.iterations):
+
+            # Select current path by ucb
+            curr_path = self._select_path(path, t, n)
+            self.search_iterations += 1
+
+            if len(curr_path) >= self.tree.depth:
+                patience += 1
+                # Break early if we keep selecting leaf nodes
+                if patience >= self.max_patience:
+                    self.early_stops += 1
+                    break
+
+            else:
+                # Create and explore child nodes
+                for c in self.tree.choices:
+                    child = curr_path + tuple(c)
+                    # Determine reward by rollout
+                    t[child] = self._roll_out(child)
+                    n[child] = 1
+                    # Back the values up
+                    self._backup(child, t)
+                    self._backup(child, n)
+
+        return self._select_choice(path, t, n)
 
     def _backup(self, path: Tuple, d: Dict[Tuple, float]) -> Dict[Tuple, float]:
         # Backup along the path
@@ -101,9 +125,10 @@ class MCTS:
         for i in range(len(path)):
             d[path[:i]] += addition
 
-    def _select_path(self, t: Dict[Tuple, float], n: Dict[Tuple, float]) -> Tuple:
+    def _select_path(
+        self, path: Tuple, t: Dict[Tuple, float], n: Dict[Tuple, float]
+    ) -> Tuple:
         # Select the current best path
-        path = tuple()
         while True:
             # Make choices until we reach an end
             try:
@@ -132,7 +157,7 @@ class MCTS:
         random_depth = self.tree.depth - len(path)
         rewards = [
             self.tree.get_reward(path + self._random_path(random_depth))
-            for _ in range(self.iterations)
+            for _ in range(self.rollouts)
         ]
 
         return max(rewards)
